95de7a46afd6668c3aa5049415d0f6987e4d16c2
RakeshRajeev <palanrakesh@gmail.com>
2025-03-05 01:29:53 +0530

    TLS

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,11 +1,13 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"  # Use the latest version
+
   cluster_name    = var.cluster_name
   cluster_version = "1.31"
   vpc_id          = var.vpc_id
   subnet_ids      = var.subnet_ids
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
   eks_managed_node_groups = {
@@ -15,16 +17,18 @@ module "eks" {
       min_capacity     = 1
       instance_types   = ["t3.medium"]
       key_name         = var.ssh_key_name
-      ami_id           = var.custom_ami_id
+      ami_id           = var.custom_ami_id  # Use `ami_id`, not `custom_ami_id`
     }
   }
-  resource "aws_iam_openid_connect_provider" "eks" {
+}
+
+# Define OIDC provider and TLS certificate OUTSIDE the module block
+data "tls_certificate" "eks" {
+  url = module.eks.cluster_oidc_issuer_url
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
   url             = module.eks.cluster_oidc_issuer_url
-  }
-
- data "tls_certificate" "eks" {
-   url = module.eks.cluster_oidc_issuer_url
-  }
 }
