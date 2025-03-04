fe4c6dd3a43be23f89d8303553a276dab5384338
RakeshRajeev <palanrakesh@gmail.com>
2025-03-04 18:29:50 +0530

    changes

---
+++ modules/addons/variables.tf
@@ -0,0 +1,35 @@
+variable "cluster_name" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "cluster_autoscaler_role_arn" {
+  type = string
+}
+
+variable "aws_lb_controller_role_arn" {
+  type = string
+}
+
+variable "karpenter_role_arn" {
+  type = string
+}
+
+variable "cert_manager_role_arn" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
+variable "rds_security_group_id" {
+  type = string
+}
