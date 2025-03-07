a461051baa47d52bb29ba4a138837a9ce747dc59
RakeshRajeev <palanrakesh@gmail.com>
2025-03-07 16:33:47 +0530

    changes

---
+++ environments/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket  = "dev-eks-terraform-state-001"
+    key     = "dev/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+    dynamodb_table = "terraform-state-locks"
+  }
+}
--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region = var.aws_region
+  profile= "my-profile"
+}
+
 module "vpc" {
   source = "../../modules/vpc"
 
@@ -12,9 +17,9 @@ module "vpc" {
 module "iam" {
   source = "../../modules/iam"
 
-  cluster_name       = var.cluster_name
-  oidc_provider_arn  = module.eks.oidc_provider_arn
-  oidc_provider      = module.eks.oidc_issuer_url
+  cluster_name = var.cluster_name
+  oidc_provider_arn   = module.eks.oidc_provider_arn
+  oidc_provider       = module.eks.oidc_issuer_url
 }
 
 module "eks" {
@@ -26,24 +31,17 @@ module "eks" {
   subnet_ids            = module.vpc.private_subnets
   eks_security_group_id = module.vpc.eks_security_group_id
   ssh_key_name          = var.ssh_key_name
+  environment           = var.environment
+  eks_worker_role_arn   = module.iam.eks_worker_role_arn
+  aws_region            = var.aws_region
+  ami_id                = var.ami_id
   private_subnets       = module.vpc.private_subnets
   node_security_group_id = module.vpc.node_security_group_id 
   inline_policy         = file("${path.module}/../../policies/eks-inline-policy.json")
-  iam_role_names        = var.iam_role_names
-  ami_id                = var.ami_id
-  eks_worker_role_arn   = module.iam.eks_worker_role_arn
-  environment           = var.environment
 }
 
 module "addons" {
   source = "../../modules/addons"
-  
-  depends_on = [module.eks]  # Add explicit dependency
-
-  providers = {
-    kubernetes = kubernetes
-    helm       = helm
-  }
 
   cluster_name                  = var.cluster_name
   aws_region                    = var.aws_region
@@ -55,30 +53,4 @@ module "addons" {
   rds_security_group_id         = module.vpc.eks_security_group_id
   cert_manager_role_arn         = module.iam.cert_manager_role_arn
   private_subnets               = module.vpc.private_subnets
-  logging_role_arn              = module.iam.logging_role_arn
-}
-
-// Manage IAM roles and policies separately
-data "aws_iam_policy_document" "eks_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "eks_override" {
-  for_each = toset(var.iam_role_names)
-  name = each.value
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
-}
-
-// Update IAM roles and policies to use aws_iam_role_policy
-resource "aws_iam_role_policy" "eks_override" {
-  for_each = toset(var.iam_role_names)
-  name     = "${each.value}-eks-policy"
-  role     = aws_iam_role.eks_override[each.key].id
-  policy   = file("${path.module}/../../policies/eks-inline-policy.json")
 }
